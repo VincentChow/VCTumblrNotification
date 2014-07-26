2758c480bc8b115248b49e2ac9b1e16fc2b5cc31
vincentxbb <vin.chow@aol.com>
2014-07-27 00:25:03 +0800

    initial build project

---
+++ VCTumblrNotification/VCTumblrNotification/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  VCTumblrNotification
+//
+//  Created by Vincent Chow on 27/7/14.
+//  Copyright (c) 2014年 Vincent Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VCTumblrNotification/VCTumblrNotification/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VCTumblrNotification
+//
+//  Created by Vincent Chow on 27/7/14.
+//  Copyright (c) 2014年 Vincent Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+    }
+}
